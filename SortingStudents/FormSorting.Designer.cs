fc56bce4b411ff317c210abaa9aafcd13060f527
Kiril Spasov <spasov.kiril@gmail.com>
2023-01-13 17:04:32 +0200

    Add project files.

---
+++ SortingStudents/FormSorting.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SortingStudents
+{
+    partial class FormSorting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSortStudents = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnSortStudents
+            // 
+            this.BtnSortStudents.Location = new System.Drawing.Point(12, 34);
+            this.BtnSortStudents.Name = "BtnSortStudents";
+            this.BtnSortStudents.Size = new System.Drawing.Size(173, 62);
+            this.BtnSortStudents.TabIndex = 0;
+            this.BtnSortStudents.Text = "Sort Students";
+            this.BtnSortStudents.UseVisualStyleBackColor = true;
+            this.BtnSortStudents.Click += new System.EventHandler(this.BtnSortStudents_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(191, 34);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtResult.Size = new System.Drawing.Size(508, 348);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormSorting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnSortStudents);
+            this.Name = "FormSorting";
+            this.Text = "Sorting Students";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSortStudents;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ SortingStudents/FormSorting.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingStudents
+{
+    public partial class FormSorting : Form
+    {
+        public FormSorting()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSortStudents_Click(object sender, EventArgs e)
+        {
+            int clases;
+            int studentsCount;
+            string[] students = new string[100];
+
+            string path = Application.StartupPath + @"\sorting.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            clases = Convert.ToInt32(streamReader.ReadLine());
+            studentsCount = Convert.ToInt32(streamReader.ReadLine());
+
+            for (int i = 0; i < clases; i++)
+            {
+                for (int j = 0; j < studentsCount; j++)
+                {
+                    students[j] = streamReader.ReadLine();
+                }
+
+                SortStudents(students, studentsCount);
+
+                TxtResult.Text += "Class #" + (i + 1) + " ordering: " + Environment.NewLine;
+
+                for (int j = 0; j < studentsCount; j++)
+                {
+                    TxtResult.Text += students[j] + Environment.NewLine;
+                }
+
+                studentsCount = Convert.ToInt32(streamReader.ReadLine());
+            }
+        }
+
+        private void SortStudents(string[] students, int studentsCount)
+        {
+            int minLocation;
+
+            for (int i = 0; i < studentsCount - 1; i++)
+            {
+                minLocation = i;
+
+                for (int j = i + 1; j < studentsCount; j++)
+                {
+                    if (!NameIsGreater(students[minLocation], students[j]))
+                    {
+                        minLocation = j;
+                    }
+                }
+                SwapStudents(students, i, minLocation);
+            }
+        }
+
+        private bool NameIsGreater(string student1, string student2)
+        {
+            bool isGreater = false;
+
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int[] student1Letters = new int[26];
+            int[] student2Letters = new int[26];
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < student1.Length; j++)
+                {
+                    if (student1[j] == alphabet[i])
+                    {
+                        student1Letters[i]++;
+                    }
+                }
+
+                for (int j = 0; j < student2.Length; j++)
+                {
+                    if (student2[j] == alphabet[i])
+                    {
+                        student2Letters[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                if (student1Letters[i] > student2Letters[i])
+                {
+                    isGreater = true;
+                    break;
+                }
+                else if (student2Letters[i] > student1Letters[i])
+                {
+                    isGreater = false;
+                    break;
+                }
+            }
+
+            return isGreater;
+        }
+
+        private void SwapStudents(string[] students, int student1Index, int student2Index)
+        {
+            string temp = "";
+            temp = students[student1Index];
+            students[student1Index] = students[student2Index];
+            students[student2Index] = temp;
+        }
+    }
+}
